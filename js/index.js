b8d6003760dabeb9bab638a5e68f86d9f3acda64
vladysanys <111743049+vladysanys@users.noreply.github.com>
2022-11-09 22:09:34 +0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,36 @@
+let sum = 10;
+const sliderLine = document.querySelector('.third-section__slider-line')
+document.querySelector('.next').addEventListener('click', () => {
+  sum = sum - 438
+  if(sum < -1314) {
+    sum = 10
+  }
+  sliderLine.style.left = sum + 'px'
+})
+document.querySelector('.prev').addEventListener('click', () => {
+  sum = sum + 438
+  if(sum > 10) {
+    sum = -1314
+  }
+  sliderLine.style.left = sum + 'px'
+})
+const search = document.querySelector('form')
+const input = document.querySelector('input')
+const sign = document.querySelector('.profile')
+const search2 = document.querySelector('.active-2')
+const debosh = document.querySelector('.debosh')
+const urna = document.querySelector('.urna')
+document.querySelector('.search').addEventListener('click', () => {
+  search.classList.remove('active')
+  input.classList.remove('active')
+  sign.classList.add('sign')
+  search2.classList.add('search-2')
+  debosh.classList.add('debosh-2')
+  input.classList.add('active-44')
+})
+document.querySelector('.urna-44').addEventListener('click', () => {
+  urna.classList.remove('urna-2')
+})
+document.querySelector('.urna_button').addEventListener('click', () => {
+  urna.classList.add('urna-2')
+})
